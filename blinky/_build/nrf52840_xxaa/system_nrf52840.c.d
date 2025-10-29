8716f4f2e8ea582538198d5c45057053d64e4b58
mkass420 <xazgaz50@gmail.com>
2025-10-29 16:29:23 +0300

    Implemented task

---
+++ blinky/_build/nrf52840_xxaa/system_nrf52840.c.d
@@ -0,0 +1,62 @@
+_build/nrf52840_xxaa/system_nrf52840.c.o: \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52840.c \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52.c \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdbool.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52840.h \
+ /home/mkass420/esl-nsdk/components/toolchain/cmsis/include/core_cm4.h \
+ /home/mkass420/esl-nsdk/components/toolchain/cmsis/include/cmsis_version.h \
+ /home/mkass420/esl-nsdk/components/toolchain/cmsis/include/cmsis_compiler.h \
+ /home/mkass420/esl-nsdk/components/toolchain/cmsis/include/cmsis_gcc.h \
+ /home/mkass420/esl-nsdk/components/toolchain/cmsis/include/mpu_armv7.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52840.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52840_bitfields.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf_peripherals.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52840_peripherals.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf_erratas.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf51_erratas.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52_erratas.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf53_erratas.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf91_erratas.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52.h \
+ /home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52_approtect.h
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52.c:
+/usr/lib/gcc/arm-none-eabi/14.2.0/include/stdint.h:
+/usr/arm-none-eabi/include/stdint.h:
+/usr/arm-none-eabi/include/machine/_default_types.h:
+/usr/arm-none-eabi/include/sys/features.h:
+/usr/arm-none-eabi/include/_newlib_version.h:
+/usr/arm-none-eabi/include/sys/_intsup.h:
+/usr/arm-none-eabi/include/sys/_stdint.h:
+/usr/lib/gcc/arm-none-eabi/14.2.0/include/stdbool.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52840.h:
+/home/mkass420/esl-nsdk/components/toolchain/cmsis/include/core_cm4.h:
+/home/mkass420/esl-nsdk/components/toolchain/cmsis/include/cmsis_version.h:
+/home/mkass420/esl-nsdk/components/toolchain/cmsis/include/cmsis_compiler.h:
+/home/mkass420/esl-nsdk/components/toolchain/cmsis/include/cmsis_gcc.h:
+/home/mkass420/esl-nsdk/components/toolchain/cmsis/include/mpu_armv7.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52840.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52840_bitfields.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf_peripherals.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52840_peripherals.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf_erratas.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf51_erratas.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf52_erratas.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf53_erratas.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/nrf91_erratas.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52.h:
+/home/mkass420/esl-nsdk/modules/nrfx/mdk/system_nrf52_approtect.h:
